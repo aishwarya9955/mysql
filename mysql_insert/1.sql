dc30682004a094c59c3cd75092e5705e2b452eb7
Aishwarya <aishwaryasingh852@gmail.com>
2025-04-26 02:06:20 +0530

    Added new patients Nishu

--- mysql_insert/1.sql
+++ mysql_insert/1.sql
@@ -1,2 +1,2 @@
-INSERT INTO patients (name, contact_no, address) VAlUES ("Ayush", 995500, "ABC"), ("Pipoh", 009988, "CDE");
+INSERT INTO patients (name, contact_no, address) VAlUES ("Ayush", 995500, "ABC"), ("Pipoh", 009988, "CDE"), ("Nishu", 76548, "tyui");
 
