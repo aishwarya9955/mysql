36329acba8867b2869f7c746f54968d016246ee9
Aishwarya <aishwaryasingh852@gmail.com>
2025-04-26 01:33:20 +0530

    This is my first sql commit

---
+++ mysql_insert/1.sql
@@ -0,0 +1,2 @@
+INSERT INTO patients (name, contact_no, address) VAlUES ("Ayush", 995500, "ABC"), ("Pipoh", 009988, "CDE");
+
---
+++ mysql_schema/1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE patients (
+    id INT(20) AUTO_INCREMENT PRIMARY KEY,  -- AUTO_INCREMENT for id
+    name VARCHAR(50),
+    contact_no INT(10),
+    address TEXT(100)
+);
+
