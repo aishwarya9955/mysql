19dacc576f8559224db7774237e91138f8ce1287
Aishwarya <aishwaryasingh852@gmail.com>
2025-04-26 17:10:52 +0530

    I created my 2nd schema

---
+++ mysql_insert/2.sql
@@ -0,0 +1,2 @@
+INSERT INTO weather_forecast VALUES (1, 25.04.25, "Pune", 04:30:14);
+
---
+++ mysql_schema/2.sql
@@ -0,0 +1,8 @@
+CREATE TABLE weather_forecast (
+	id INT PRIMARY KEY,
+	date INT,
+	place VARCHAR(50),
+	time INT
+);
+
+	
